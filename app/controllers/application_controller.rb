23e33d76d264ed9a9969e1cfb3b11b98d826660a
LaNice Powell <lanicemp@aol.com>
2019-11-11 11:01:48 -0500

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,48 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "auth_demo_lv"
   end
 
-  get "/" do
-    erb :welcome
+  helpers do 
+    
+    def logged_in?
+      !!sesssion[:email]
+    end 
+
+    def login(email, password)
+      
+      student = StudentUser.find_by(:email => email)
+    
+      if student &&  user.authenticate(password)
+      session[:email] = student.email
+      else 
+        redirect '/login'
+      end 
+    end 
+
+    def logout!
+      session.clear
+    end 
+
   end
 
-end
+end 
+
+
+    # get "/" do
+    #   erb :welcome
+    # end
+
+    # get "/login" do 
+    #   erb :"sessions/login.html"
+    # end 
+
+    # get "/create_account" do 
+    #   erb :"sessions/create_account"
+    # end 
+
+
+      
+
