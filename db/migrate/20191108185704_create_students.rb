73d082af4b11df17d5d6540b24f720de9b8a3a1e
LaNice Powell <lanicemp@aol.com>
2019-11-12 22:13:03 -0500

    updated

---
+++ db/migrate/20191108185704_create_students.rb
@@ -0,0 +1,13 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :age
+      t.string :grade
+      t.string :username
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
+  end
+end
