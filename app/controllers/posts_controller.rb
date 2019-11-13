4bda1593a19106bf9cfee8e27104e87a2994a95c
LaNice Powell <lanicemp@aol.com>
2019-11-12 22:22:09 -0500

    obsolete controller soon to be deleted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,35 @@
-class PostsController < ApplicationController
+##Delete this later using as reference 
 
-    get '/' do
-        redirect '/posts'
-    end 
 
-    get '/post' do 
-        status 200 
-        @post = Post.visible 
-        erb "posts/index.html"
-    end 
+# class PostsController < ApplicationController
 
-    get '/post/new' do 
-        if !logged_in?
-            redirect "login"
-        else 
-            "A new post form"
-        end
-    end 
+#     get '/' do
+#         redirect '/posts'
+#     end 
 
-    get 'posts/:id' do 
-        @post = Post.find(paramas[:id])
-    end 
+#     get '/post' do 
+#         status 200 
+#         @post = Post.visible 
+#         erb :"posts/index.html"
+#     end 
+
+#     get '/post/new' do 
+#         if !logged_in?
+#             redirect "login"
+#         else 
+#             "A new post form"
+#         end
+#     end 
+
+#     get 'posts/:id' do 
+#         @post = Post.find(paramas[:id])
+#         erb :"posts/show.html"
+#     end 
+
+#     post '/posts' do 
+#         @post = Post.create(title: params[:title], content: params[:content])
+#         redirect "/posts/#{@posts.id}"
+#     end 
 
     
-end 
+# end 
