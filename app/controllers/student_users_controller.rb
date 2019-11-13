b76995e6a3a0fc24301b484d5a0555274bf00a25
LaNice Powell <lanicemp@aol.com>
2019-11-12 22:23:36 -0500

    updated the methods

--- app/controllers/student_users_controller.rb
+++ app/controllers/student_users_controller.rb
@@ -1,23 +1,37 @@
 class StudentUsersController < ApplicationController
     
-    configure do 
-        set :views, 'app/views'
-        set :sessions, true
-    end 
     
-    get 'signup' do 
-        erb :"students/create_account"
-    end 
+  get '/signup' do 
+      erb :"student_users/create_account.html"
+  end 
 
-    post 'students' do 
-        @student = StudentUser.new 
-        @student.email = params[:email]
-        @student.password = params[:password]
-        if @student.save 
-            redirect '/login'
-        else 
-            erb :"/signup"
-        end 
-    end 
+  post "/create_account" do
+    user = StudentUser.new(
+      :name => params[:name],
+      :email => params[:email], 
+      :username => params[:username], 
+      :password => params[:password],
+      :age => params[:age],
+      :grade => params[:grade]) 
+
+    if user.password == params[:password_confirmation] && user.save
+      login(user.email, user.password)
+    
+      redirect "/questions"
+    else
+      redirect "/failure"
+    end
+  end 
+    
+  # post 'students' do 
+  #   @student = StudentUser.new 
+  #   @student.email = params[:email]
+  #   @student.password = params[:password]
+  #   if @student.save 
+  #     redirect '/login'
+  #   else 
+  #     erb :"/signup"
+  #   end 
+  # end 
 
 end 
