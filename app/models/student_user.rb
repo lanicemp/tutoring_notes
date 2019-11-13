ca7f35da18c6e3b8bf8c3240f378d2610c806318
LaNice Powell <lanicemp@aol.com>
2019-11-12 22:40:33 -0500

    updated

--- app/models/student_user.rb
+++ app/models/student_user.rb
@@ -1,10 +1,13 @@
 class StudentUser < ActiveRecord::Base
 
     has_many :questions 
-    has_many :comments 
+    has_many :answers
+    has_many :received_answers, through: :questions, source: :answer
+    #looking for recieved answers through the questions that I asked and looking
+    # in the answer association for each question. 
 
-    validates :name, presence: true
-    validates :email, uniqueness: true 
+    validates :name, :email, presence: true
+    validates :email, :username,  uniqueness: true 
     has_secure_password
     
     
