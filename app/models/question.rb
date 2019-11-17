70249ba984d8e7aa7f14a9523c2fe5cbdb6ce479
LaNice Powell <lanicemp@aol.com>
2019-11-17 13:36:43 -0500

    added presence

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,6 @@ class Question < ActiveRecord::Base
     belongs_to :student_user
     has_many :answers
 
-    validates :inquiry, uniqueness: true
+    validates :inquiry, presence:true, uniqueness: true
 
 end
